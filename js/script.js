3b59a1c32393f277fdd5bd47a7a9faeffcd56966
jdh1999 <jackhettinger@comcast.net>
2020-02-25 12:13:23 -0500

    try

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 
+
+function startUp() {
+    var btnErin = document.getElementById("btnErin");
+    var clickErin = document.getElementById("btnErin");
+    btnErin.addEventListener("touchstart", function pop_One(start){
+        var touch = start.changedTouches[0];
+
+        if (touch == btnErin){
+            newwindow=window.open('https://www.google.com/maps','name','height=400,width=600');
+	        if (window.focus) {newwindow.focus()}
+        }
+
+        alert(this.id + "tocuh");
+    }, false);
+
+}
+
 var newwindow;
 function pop_new_window(url)
 {
-	newwindow=window.open(url,'name','height=400,width=200');
+	newwindow=window.open(url,'name','height=400,width=600');
 	if (window.focus) {newwindow.focus()}
 }
